a68a546b82716d485a62e28e0cdfade6d2e565a9
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-04-30 12:41:10 +0000

    Add validations to BankOfGhanaRate model

--- app/models/bank_of_ghana_rate.rb
+++ app/models/bank_of_ghana_rate.rb
@@ -1,5 +1,6 @@
+# This model allows is used for storing the TBill rates by bank of Ghana
+# which change each week.
 class BankOfGhanaRate < ApplicationRecord
-
 
   validates :discount_rate_91_days,
             :interest_rate_91_days,
@@ -7,4 +8,14 @@ class BankOfGhanaRate < ApplicationRecord
             :interest_rate_182_days,
             :start_date,
             :end_date, presence: true
+
+  validates :discount_rate_91_days,
+            :interest_rate_91_days,
+            :discount_rate_182_days,
+            :interest_rate_182_days,
+            # Length should not be more than 6 digits
+            length: { is: 6, message: 'rates should be 6 digits' },
+            # Should be an integer greater than zero
+            numericality: { only_integer: true, greater_than: 0 }
+
 end
