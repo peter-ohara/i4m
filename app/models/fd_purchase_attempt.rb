400e1ced7e6e0378fcf9c522b1d68aa0dd91911f
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-05-20 09:54:57 +0000

    Monetize appriopriate fields in the models

--- app/models/fd_purchase_attempt.rb
+++ app/models/fd_purchase_attempt.rb
@@ -1,4 +1,6 @@
 class FdPurchaseAttempt < ApplicationRecord
   belongs_to :user
   belongs_to :fixed_deposit_investment
+
+  monetize :principal_pesewas, as: 'principal'
 end
--- app/models/fixed_deposit_investment.rb
+++ app/models/fixed_deposit_investment.rb
@@ -1,6 +1,8 @@
 class FixedDepositInvestment < ApplicationRecord
   belongs_to :institution
 
+  monetize :minimum_principal_pesewas, as: 'minimum_principal'
+
   def interest_rate
     FixedDepositRate.find_by(fixed_deposit_investment: self).interest_rate
   end
