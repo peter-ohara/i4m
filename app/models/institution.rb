f686355f031fd5f97ac4de53cfd1faeedcd64609
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-05-20 17:03:53 +0000

    Add has_many associations

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,2 +1,4 @@
 class Institution < ApplicationRecord
+  has_many :fixed_deposit_investments, inverse_of: :institution
+  has_many :mutual_funds, inverse_of: :institution
 end
--- app/models/mutual_fund.rb
+++ app/models/mutual_fund.rb
@@ -1,6 +1,9 @@
 class MutualFund < ApplicationRecord
   belongs_to :institution
 
+  has_many :mutual_fund_rates, inverse_of: :mutual_fund
+  has_many :mutual_fund_purchase_attempts, inverse_of: :mutual_fund
+
   monetize :minimum_principal_pesewas, as: 'minimum_principal'
   monetize :debit_order_pesewas, as: 'debit_order'
 
